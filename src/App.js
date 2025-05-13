54f7d4ff3d8fb56780d7536c3c21c2bfb4482bef
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-13 16:26:00 -0700

    set up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Root }from "./components/root/Root"
+import { Index } from './components/index/Index';
+import { Map } from './components/map/Map';
+
 import './App.css';
+
+
+const router = 
+
+createBrowserRouter( createRoutesFromElements(
+  <Route 
+    path="/" 
+    element={<Root/>}>
+        <Route 
+          path="map/:component" 
+          element={ <Map/>}/>
+        <Route 
+          path="index" 
+          element={<Index/> }/>
+  </Route>
+))
 
 function App() {
   return (
-  <div>
-    
-  </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
