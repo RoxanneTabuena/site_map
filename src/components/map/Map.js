29a7e652a7daf9974afa4e8f6771dcf82d95ab2d
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-14 13:41:48 -0700

    add data to map component

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,27 @@
-import { useParams } from 'react-router-dom'
-import { Component } from './component/Component'
+import { useParams, NavLink } from 'react-router-dom'
+import { content_dic } from '../site_info'
 import style from './map.module.css'
 export const Map = () => {
     const {component} = useParams()
-    return ( <div>
-        <Component component={component.slice(1)}/>
+    const id = component.slice(1)
+
+    let sxatic = []
+    let links = []
+    content_dic[id].forEach((item)=>{
+            if(typeof(item)=== 'object'){   
+                const [[itemContent , link]] = Object.entries(item)
+                links.push(<div className={style.connect}>
+                    <p>{itemContent}</p>
+                    <NavLink to={`/map/:${link}`}>{link}</NavLink>
+                </div>)
+            }else{
+                sxatic.push(<p>{item}</p>)
+            }
+        })
+    return ( 
+    <div className={style.map}>
+        <h1>{id}</h1>
+        {sxatic}
+        {links}
     </div>)
 }
