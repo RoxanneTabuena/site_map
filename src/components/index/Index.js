7761ee1dc948808cf56b66ce19a834951e1b19ab
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-13 17:58:19 -0700

    update index style

--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -2,10 +2,13 @@ import { NavLink } from 'react-router-dom'
 import { site_dic } from '../site_info'
 import style from './index.module.css'
 export const Index = () => {
-    return (<div>
-{        Object.keys(site_dic).map(page => {
+    let links = Object.keys(site_dic).map(page => {
             return <NavLink to={`/map/:${page}`} key={page}>{page}</NavLink>
-        })}
-        
+        })
+    return (
+    <div className={style.index}>
+        <h1>Complete Page Index</h1>
+        {links}
+        <p>*The presence of parameters (ie ':service') indicate a variable. These pages are actually templates that will result in serval pages. On your final site, the actual path name will look more like: "services/weddings" and the related page will serve up relevant information based on the given parameters.</p>
     </div>)
 }
