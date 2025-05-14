e7d3441bc6562d8f7ca0d3f36cadbbe85647dce0
Roxanne Tabuena <roxyannet@gmail.com>
2025-05-14 14:11:29 -0700

    Manicure Map Content

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,5 +1,6 @@
 import { useParams, NavLink } from 'react-router-dom'
 import { content_dic, summary_dic } from '../site_info'
+import { capitalize } from '../utils'
 import style from './map.module.css'
 export const Map = () => {
     const {component} = useParams()
@@ -11,17 +12,18 @@ export const Map = () => {
             if(typeof(item)=== 'object'){   
                 const [[itemContent , link]] = Object.entries(item)
                 links.push(<div className={style.connect}>
-                    <p>{itemContent}</p>
-                    <NavLink to={`/map/:${link}`}>{link}</NavLink>
+                    <p>{capitalize(itemContent)}</p>
+                    <NavLink to={`/map/:${link}`}>{capitalize(link)}</NavLink>
                 </div>)
             }else{
-                sxatic.push(<p>{item}</p>)
+                sxatic.push(<p>{capitalize(item)}</p>)
             }
         })
     return ( 
     <div className={style.map}>
-        <h1>{id}</h1>
+        <h1>{`${capitalize(id)}`}</h1>
         <p>{summary_dic[id]}</p>
+        <h2>Content:</h2>
         {sxatic}
         {links}
     </div>)
